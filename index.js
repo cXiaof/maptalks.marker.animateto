e865373bafca801eb3dfa2f373a19e5100da53b4
cXiaof <310257509@qq.com>
2021-07-08 14:38:59 +0800

    update package

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 import * as maptalks from 'maptalks'
 
+const uid = 'routetopo@cXiaof'
 const options = {}
 
 export class Routetopo extends maptalks.Class {
     constructor(options) {
         super(options)
+        this._layerName = `${maptalks.INTERNAL_LAYER_PREFIX}${uid}`
     }
 }
 
